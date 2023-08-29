18b7f341d903dcd1a7580a2bf4a4a5db43dd491c
Mathew Bravo <mathew.bravo@wilyglobal.com>
2023-08-29 15:50:58 -0400

    Worked up to parsers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/MathewBravo/mblang/pkg/repl"
+)
+
 func main() {
-		
+	fmt.Println("Welcome to the MBLang Programming Language")
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
 }
